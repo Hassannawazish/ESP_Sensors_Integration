1b34031f5e96342c8f0cf6b8c659c27e66c1137b
HassanNawazish <hassannawazishrasool1@gmail.com>
2023-08-31 10:48:31 +0500

    dust_sensor integration

--- main_combination.ino
+++ main_combination.ino
@@ -1,6 +1,8 @@
 #include <Arduino.h>
 #include <Adafruit_Sensor.h>
 #include <DHT.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
 
 #define DHTPIN 16      // GPIO16 (D0)
 #define DHTTYPE DHT11
@@ -14,6 +16,15 @@ DHT dht(DHTPIN, DHTTYPE);
 float sensorValue;
 
 const int digitalPin_door = 34;
+
+int measurePin = A3;
+int ledPower = 12;
+unsigned int samplingTime = 280;
+unsigned int deltaTime = 40;
+unsigned int sleepTime = 9680;
+float voMeasured = 0;
+float calcVoltage = 0;
+float dustDensity = 0;
 
 void taskTemperatureHumidity(void *pvParameters) {
   for (;;) {
@@ -72,6 +83,33 @@ void taskDoorStatus(void *pvParameters) {
   }
 }
 
+void taskAdditionalSensor(void *pvParameters) {
+  for (;;) {
+    digitalWrite(ledPower, LOW);
+    delayMicroseconds(samplingTime);
+    voMeasured = analogRead(measurePin);
+    delayMicroseconds(deltaTime);
+    digitalWrite(ledPower, HIGH);
+    delayMicroseconds(sleepTime);
+    voMeasured = map(voMeasured, 0, 4095, 0, 1023);
+    calcVoltage = voMeasured * (5.0 / 1024);
+    dustDensity = 0.17 * calcVoltage - 0.1;
+
+    if (dustDensity < 0) {
+      dustDensity = 0.00;
+    }
+
+    Serial.print("Raw Signal Value (0-1023):  ");
+    Serial.print(voMeasured);
+    Serial.print("     Voltage:   ");
+    Serial.print(calcVoltage);
+    Serial.print("     Dust Density:   ");
+    Serial.println(dustDensity);
+
+    vTaskDelay(pdMS_TO_TICKS(2000));
+  }
+}
+
 void setup() {
   pinMode(digitalPin_door, INPUT);
   pinMode(sensor_DO, INPUT);
@@ -79,10 +117,11 @@ void setup() {
 
   dht.begin();
 
-  xTaskCreate(taskTemperatureHumidity, "TempHumTask", 1000, NULL, 1, NULL);
+  xTaskCreate(taskTemperatureHumidity, "TempHumTask", 1000, NULL, 2, NULL);
   xTaskCreate(taskDigitalOutput, "DigitalOutTask", 1000, NULL, 1, NULL);
   xTaskCreate(taskSmokeDetection, "SmokeDetectTask", 1000, NULL, 1, NULL);
   xTaskCreate(taskDoorStatus, "DoorStatusTask", 1000, NULL, 1, NULL);
+  xTaskCreate(taskAdditionalSensor, "AdditionalSensorTask", 1000, NULL, 2, NULL);
 
   vTaskStartScheduler();
 }
